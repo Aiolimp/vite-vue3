bedaa99086b483bbd02f0ced75baef364a12f76e
Aiolimp <172978010@qq.com>
2021-04-29 17:04:58 +0800

    :rocket: 架构搭建

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
+// 有关antdUi组件
+import "ant-design-vue/dist/antd.css";
+import Antd from "ant-design-vue";
 
 createApp(App).mount('#app')
+createApp(App).use(router, store,Antd).mount('#app')
+// createApp(App).use(store).mount('#app')
---
+++ src/views/axios.vue
@@ -0,0 +1,19 @@
+<template></template>
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import axios from '../utils/axios'
+
+  export default defineComponent({
+    setup() {
+        //使用axios
+      axios
+        .get('/users/XPoet')
+        .then((res) => {
+          console.log('res: ', res)
+        })
+        .catch((err) => {
+          console.log('err: ', err)
+        })
+    }
+  })
+</script>
